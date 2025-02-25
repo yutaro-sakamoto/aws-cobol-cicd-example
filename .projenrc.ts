380eac0e84fa4aaa4f27cb5a6c59a5eada69b774
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-25 22:14:07 +0900

    ci: add a deploy pipeline

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,6 @@
 import { AwsCdkTypeScriptApp } from '@yutaro-sakamoto/projen-cdk';
+import { YamlFile } from 'projen';
+
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
@@ -13,5 +15,110 @@ const project = new AwsCdkTypeScriptApp({
 
 project.addDeps('dotenv');
 project.gitignore.addPatterns('.env');
+
+
+// Deploy to AWS
+new YamlFile(project, '.github/workflows/push-dev.yml', {
+  obj: {
+    name: 'push',
+    on: {
+      push: {
+        branches: ['dev'],
+      },
+    },
+    concurrency: {
+      'group': '${{ github.workflow }}-${{ github.ref }}',
+      'cancel-in-progress': true,
+    },
+    permissions: {
+      contents: 'read',
+    },
+    jobs: {
+      'check-workflows': {
+        permissions: {
+          contents: 'read',
+        },
+        uses: './.github/workflows/check-workflows.yml',
+      },
+      'test': {
+        needs: 'check-workflows',
+        permissions: {
+          contents: 'read',
+        },
+        secrets: 'inherit',
+        uses: './.github/workflows/deploy.yml',
+        with: {
+          environment: 'dev',
+        }
+      },
+    },
+  },
+});
+
+// Deploy to AWS
+new YamlFile(project, '.github/workflows/deploy.yml', {
+  obj: {
+    name: 'deploy',
+    on: {
+      workflow_call: {
+        inputs: {
+          environment: {
+            type: 'string',
+            required: true,
+            description: 'Environment to deploy to',
+          }
+        }
+      }
+    },
+    permissions: {
+      contents: 'read',
+      'id-token': 'write',
+    },
+    jobs: {
+      deploy: {
+        'runs-on': 'ubuntu-latest',
+        environment: '${{ inputs.environment }}',
+        steps: [
+          {
+            uses: 'aws-actions/configure-aws-credentials@v4',
+            with: {
+              'role-to-assume': 'arn:aws:iam::${{ secrets.AWS_ID }}:role/${{ secrets.ROLE_NAME }}',
+              'role-session-name': 'gh-oidc-${{ github.run_id }}-${{ github.run_attempt }}',
+              'aws-region': '${{ secrets.AWS_REGION }}',
+            },
+          },
+          {
+            name: 'Checkout',
+            uses: 'actions/checkout@v4',
+          },
+          {
+            uses: 'actions/setup-node@v4',
+            with: {
+              'node-version': '22',
+              cache: 'npm',
+              'cache-dependency-path': 'package-lock.json',
+            },
+          },
+          {
+            run: 'npm ci',
+          },
+          {
+            name: 'Set environment variabble CDK_DEFAULT_REGION',
+            run: 'echo "export CDK_DEFAULT_REGION=${{ secrets.AWS_REGION }}" >> $GITHUB_ENV',
+          },
+          {
+            name: 'Set environment variabble CDK_DEFAULT_ACCOUNT',
+            run: 'echo "export CDK_DEFAULT_ACCOUNT=${{ secrets.AWS_ID }}" >> $GITHUB_ENV',
+          },
+          {
+            name: 'Deploy',
+            run: 'npx cdk deploy --require-approval never',
+          },
+        ],
+      },
+    },
+  },
+});
+
 
 project.synth();
