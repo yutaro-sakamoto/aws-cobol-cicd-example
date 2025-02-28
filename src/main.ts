b1227705987cd66fe4e19926c56ee326528c2570
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-03-01 00:45:28 +0900

    feat: add a Codepipeline

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { App, Stack, StackProps } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import * as ecr from "aws-cdk-lib/aws-ecr";
-import { AwsSolutionsChecks } from "cdk-nag";
+import { AwsSolutionsChecks, NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
 import * as dotenv from "dotenv";
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 
 dotenv.config();
 
@@ -17,6 +19,45 @@ export class EcrStack extends Stack {
     new ecr.Repository(this, "ECRRepository", {
       repositoryName: "my-ecr-repo",
     });
+
+    const pipeline = new codepipeline.Pipeline(this, "ApplicationPipeline", {
+      pipelineName: "ApplicationPipeline",
+    });
+
+    NagSuppressions.addResourceSuppressions(
+      pipeline.artifactBucket.encryptionKey!,
+      [
+        {
+          id: "AwsSolutions-KMS5",
+          reason: "This key is used for the artifact bucket.",
+        },
+      ],
+    );
+
+    const sourceOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: "GitHubSource",
+          owner: "yutaro-sakamoto",
+          repo: "aws-cobol-cicd-example",
+          output: sourceOutput,
+          oauthToken: cdk.SecretValue.unsafePlainText(""),
+          trigger: codepipeline_actions.GitHubTrigger.NONE,
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new codepipeline_actions.ManualApprovalAction({
+          actionName: "ManualApproval",
+        }),
+      ],
+    });
     // define resources here...
   }
 }
@@ -29,7 +70,13 @@ const devEnv = {
 
 const app = new App();
 
-new EcrStack(app, "aws-cobol-cicd-example-dev", { env: devEnv });
+const stack = new EcrStack(app, "aws-cobol-cicd-example-dev", { env: devEnv });
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
-
+NagSuppressions.addStackSuppressions(stack, [
+  { id: "AwsSolutions-IAM5", reason: "Allow IAM policies to contain *" },
+  {
+    id: "AwsSolutions-S1",
+    reason: "Server access logs of S3 bucket are unnecessary",
+  },
+]);
 app.synth();
