b9f6695853571eec3657c7d7ba010433b72a288c
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-03-21 20:53:38 +0900

    chore: remove unused variables

--- src/constants.ts
+++ src/constants.ts
@@ -7,14 +7,3 @@ export const ecrRepositoryName = "my-ecr-repo";
  */
 export const taskDefinitionArnSsmParamName =
   "/ecs-cicd-example/task-definition-arn";
-
-/**
- * The SSM parameter name indicating the Fargate service ARN
- */
-export const fargateServiceArnParamName =
-  "/ecs-cicd-example/fargate-service-arn";
-
-/**
- * The SSM parameter name indicating the ECS cluster ARN
- */
-export const clusterArnParamName = "/ecs-cicd-example/cluster-arn";
