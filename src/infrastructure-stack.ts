8bee6871cde32fc8afbda6b37bf1c5f0f3a7b28e
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-03-24 17:40:56 +0900

    feat: add alb

--- src/infrastructure-stack.ts
+++ src/infrastructure-stack.ts
@@ -6,6 +6,10 @@ import * as logs from "aws-cdk-lib/aws-logs";
 import * as ssm from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
 import * as constants from "./constants";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { NagSuppressions } from "cdk-nag";
 
 /**
  * Stack props
@@ -26,7 +30,22 @@ export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props: InfrastructureStackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "VPC", {});
+    const vpc = new ec2.Vpc(this, "VPC", {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+      maxAzs: 2,
+    });
 
     // Create VPC endpoints
     vpc.addInterfaceEndpoint("ECREndpoint", {
@@ -81,12 +100,63 @@ export class InfrastructureStack extends Stack {
       image: ecs.ContainerImage.fromRegistry(
         "registry.hub.docker.com/ealen/echo-server",
       ),
+      portMappings: [
+        {
+          containerPort: 80,
+        },
+      ],
       logging: ecs.LogDrivers.awsLogs({ streamPrefix: "ecs" }),
     });
 
+    // Create security groups
+    const albSecurityGroup = new ec2.SecurityGroup(this, "AlbSecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    albSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allow HTTP traffic",
+    );
+
+    albSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      "Allow HTTPS traffic",
+    );
+
+    // add nag suppression
+    NagSuppressions.addResourceSuppressions(albSecurityGroup, [
+      {
+        id: "AwsSolutions-EC23",
+        reason: "Allow all inbound traffic to ALB",
+      },
+    ]);
+
+    const fargateSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "FargateSecurityGroup",
+      {
+        vpc,
+        allowAllOutbound: true,
+      },
+    );
+
+    fargateSecurityGroup.addIngressRule(
+      albSecurityGroup,
+      ec2.Port.tcp(80),
+      "Allow HTTP traffic from ALB",
+    );
+
+    // Create ECS service
     this.fargateService = new ecs.FargateService(this, "FargateService", {
       cluster,
       taskDefinition: defaultTaskDefinition,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      securityGroups: [fargateSecurityGroup],
     });
 
     (
@@ -97,5 +167,41 @@ export class InfrastructureStack extends Stack {
           this,
           constants.taskDefinitionArnSsmParamName,
         );
+
+    // Create ALB
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "LB", {
+      vpc,
+      internetFacing: true,
+      securityGroup: albSecurityGroup,
+    });
+
+    // Logging settings
+    const logBucket = new s3.Bucket(this, "LogBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      enforceSSL: true,
+    });
+
+    alb.logAccessLogs(logBucket);
+
+    const listener = alb.addListener("Listener", {
+      port: 80,
+    });
+
+    listener.addTargets("ECS", {
+      port: 80,
+      targets: [this.fargateService],
+      healthCheck: {
+        path: "/",
+        interval: cdk.Duration.minutes(1),
+      },
+    });
+
+    // ALBのDNS名を出力
+    new cdk.CfnOutput(this, "LoadBalancerDNS", {
+      value: alb.loadBalancerDnsName,
+      description: "LoadBalancer DNS",
+    });
   }
 }
